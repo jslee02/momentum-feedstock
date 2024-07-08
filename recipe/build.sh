00f968f301e3e0a194cba59df113305ec96d1a95
Jeongseok Lee <jeongseok@meta.com>
2024-07-08 05:34:03 -0700

    Enable building tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,12 +14,16 @@ cmake $SRC_DIR \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
   -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON \
-  -DMOMENTUM_BUILD_TESTING=OFF \
+  -DMOMENTUM_BUILD_TESTING=ON \
   -DMOMENTUM_BUILD_EXAMPLES=OFF \
   -DMOMENTUM_BUILD_PYMOMENTUM=OFF \
   -DMOMENTUM_BUILD_WITH_EZC3D=OFF \
   -DMOMENTUM_BUILD_WITH_OPENFBX=OFF
 
 cmake --build build --parallel
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  ctest --test-dir build --output-on-failure
+fi
 
 cmake --build build --parallel --target install
