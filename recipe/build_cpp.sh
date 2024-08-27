195307dd300b71bbba8b8e941803c0130190e818
Jeongseok Lee <jeongseok@meta.com>
2024-08-26 18:10:47 -0700

    Build aarch64

--- recipe/build_cpp.sh
+++ recipe/build_cpp.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -exo pipefail
+
+# Display environment info
+echo CONDA_BUILD_CROSS_COMPILATION: $CONDA_BUILD_CROSS_COMPILATION
+echo CROSSCOMPILING_EMULATOR      : $CROSSCOMPILING_EMULATOR
+echo build_platform               : $build_platform
+echo target_platform              : $target_platform
 
 if [[ "${target_platform}" == osx-* ]]; then
   # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
   CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" || "${target_platform}" == osx-* ]]; then
-  MOMENTUM_ENABLE_SIMD=OFF
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" || "${target_platform}" == osx-* ]]; then
+  momentum_enable_simd=OFF
 else
-  MOMENTUM_ENABLE_SIMD=ON
+  momentum_enable_simd=ON
 fi
 
 # Workaround for fx/gltf.h:70:13: error: narrowing conversion of '-1' from 'int' to 'char' [-Wnarrowing]
@@ -18,11 +24,11 @@ if [[ "${target_platform}" == *aarch64 || "${target_platform}" == *ppc64le ]]; t
   CXXFLAGS="${CXXFLAGS} -Wno-narrowing"
 fi
 
-# Disable use of system-installed GTest libraries when cross-compiling
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
-  MOMENTUM_USE_SYSTEM_GOOGLETEST=ON
+# Disable tests when cross-compiling
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  build_testing=OFF
 else
-  MOMENTUM_USE_SYSTEM_GOOGLETEST=OFF
+  build_testing=ON
 fi
 
 cmake $SRC_DIR \
@@ -33,10 +39,10 @@ cmake $SRC_DIR \
   -DCMAKE_BUILD_TYPE=Release \
   -DMOMENTUM_BUILD_EXAMPLES=OFF \
   -DMOMENTUM_BUILD_PYMOMENTUM=OFF \
-  -DMOMENTUM_BUILD_TESTING=ON \
-  -DMOMENTUM_ENABLE_SIMD=$MOMENTUM_ENABLE_SIMD \
-  -DMOMENTUM_USE_SYSTEM_GOOGLETEST=$MOMENTUM_USE_SYSTEM_GOOGLETEST \
-  -DMOMENTUM_USE_SYSTEM_PYBIND11=OFF \
+  -DMOMENTUM_BUILD_TESTING=$build_testing \
+  -DMOMENTUM_ENABLE_SIMD=$momentum_enable_simd \
+  -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON \
+  -DMOMENTUM_USE_SYSTEM_PYBIND11=ON \
   -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
 
 cmake --build build --parallel
--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -exo pipefail
+
+# Display environment info
+echo CONDA_BUILD_CROSS_COMPILATION: $CONDA_BUILD_CROSS_COMPILATION
+echo CROSSCOMPILING_EMULATOR      : $CROSSCOMPILING_EMULATOR
+echo build_platform               : $build_platform
+echo target_platform              : $target_platform
 
 # Set CMake options for pymomentum
+if [[ "${target_platform}" == *aarch64 || "${target_platform}" == *ppc64le ]]; then
+  export MOMENTUM_BUILD_TESTING=OFF
+fi
 export MOMENTUM_ENABLE_SIMD=OFF
 export MOMENTUM_USE_SYSTEM_PYBIND11=OFF
+
+# Workaround for fx/gltf.h:70:13: error: narrowing conversion of '-1' from 'int' to 'char' [-Wnarrowing]
+if [[ "${target_platform}" == *aarch64 || "${target_platform}" == *ppc64le ]]; then
+  CXXFLAGS="${CXXFLAGS} -Wno-narrowing"
+fi
 
 # Install the current package with verbose output
 python -m pip install . -vv
