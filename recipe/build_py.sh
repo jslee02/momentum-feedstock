ac964d80372bdaded378028630119488ece742b9
Jeongseok Lee <jeongseok@meta.com>
2024-10-04 17:51:51 -0700

    Simplify build script

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -8,24 +8,6 @@ export MOMENTUM_ENABLE_SIMD=OFF
 # Install the current package with verbose output
 python -m pip install . -vv
 
-# Get the Python site-specific directory path for platform-dependent files
-PYTHON_SITEARCH=$(python3 -c "import sysconfig; print(sysconfig.get_path('platlib'))")
-
-# Remove the CONDA_PREFIX part from PYTHON_SITEARCH to get the relative path
-RELATIVE_PATH=${PYTHON_SITEARCH#"$CONDA_PREFIX"}
-RELATIVE_PATH=${RELATIVE_PATH#/}
-
-# Construct the new path using PREFIX
-MERGED_PATH="$PREFIX/$RELATIVE_PATH"
-
-# For debugging
-echo "PREFIX         = $PREFIX"
-echo "CONDA_PREFIX   = $CONDA_PREFIX"
-echo "PYTHON_SITEARCH= $PYTHON_SITEARCH"
-echo "RELATIVE_PATH  = $RELATIVE_PATH"
-echo "MERGED_PATH    = $MERGED_PATH"
-echo "SP_DIR         = $SP_DIR"
-
 # Copy all .so files to the target directory except those containing 'test' in the filename
 mkdir -p "$SP_DIR/pymomentum"
 find pymomentum -name "*.so" ! -name "*test*" -exec cp {} "$SP_DIR/pymomentum" \;
