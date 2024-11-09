9cdeb5cca3bb5a00a0b30a967bebceafa7062fc6
Jeongseok Lee <jeongseok@meta.com>
2024-11-08 17:34:53 -0800

    MOMENTUM_BUILD_IO_FBX=OFF

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 python -m pip install . -vv \
     --global-option=build_ext \
     --global-option=--cmake-args='
-        -DMOMENTUM_BUILD_IO_FBX=ON
+        -DMOMENTUM_BUILD_IO_FBX=OFF
         -DMOMENTUM_BUILD_EXAMPLES=OFF
         -DMOMENTUM_BUILD_TESTING=ON
         -DMOMENTUM_ENABLE_SIMD=$MOMENTUM_ENABLE_SIMD
