1cf56f3ea09c442aa76e8097ba2e366f5167a526
Jeongseok Lee <jeongseok@meta.com>
2025-03-25 08:13:14 -0700

    Enable pymomentum for windows

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,3 +1,10 @@
 @echo on
 
+nvcc --version
+
+set CMAKE_ARGS=%CMAKE_ARGS% ^
+    -DMOMENTUM_USE_SYSTEM_GOOGLETEST=ON ^
+    -DMOMENTUM_USE_SYSTEM_PYBIND11=ON ^
+    -DMOMENTUM_USE_SYSTEM_RERUN_CPP_SDK=ON
+
 python -m pip install --no-deps --ignore-installed . -vv --prefix=%PREFIX%
